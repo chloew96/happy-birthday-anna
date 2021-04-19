cd9ed672ccdb29fc0359067052fdd9ab97a8eacb
chloew96 <chloew090600@gmail.com>
2021-04-18 23:49:35 -0400

    first commit, deploy react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './images/card.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='date'>
+        0416/2021
+      </div>
+      <div className="app-container">
+        ✨🎊 Happy birthday Anna 🎉✨
+        <img src={Card} className="card"/>
+      </div>
+      
     </div>
   );
 }
